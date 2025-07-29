1bee565a84ed1c049e56c029eebfeda8b2c7e4e7
“pank886” <1134926811@qq.com>
2025-07-29 16:29:46 +0800

    修改了test_selenium，调用了封装的方法

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,30 +1,19 @@
-#import requests
-from time import sleep
 from my_package.common_imports import *
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from peges.login_page import LoginPage
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.close()
+
 
 @pytest.mark.web
-def test_web(browser):
-    browser.get('http://192.168.1.55:6905/login')
-    wiit = WebDriverWait(browser, 10)
-    print(browser.title)
+def test_web(driver):
+    login_page = LoginPage(driver)
+    login_page.open()
 
-    username_field = browser.find_element(By.XPATH, '//*[@placeholder="请输入账号"]')
-    password_field = browser.find_element(By.XPATH, '//*[@placeholder="请输入密码"]')
-    verification_field = browser.find_element(By.XPATH, '//*[@placeholder="请输入验证码"]')
-    login_button = browser.find_element(By.XPATH, '//*[@class="el-button login-btn"]')
+    login_page.login('admin', 'abc12345')
 
-#自动输入账密
-    username_field.send_keys('admin')
-    password_field.send_keys('abc12345')
-#手动输入验证码
-    verification = input("输入验证码：")
-    verification_field.send_keys('verification')
-
-
-    login_button.click()
-
-    assert "吉大正元智慧园区管理平台" in browser.title
+    assert login_page.is_title_contains("吉大正元"), "登录失败，标题错误"
